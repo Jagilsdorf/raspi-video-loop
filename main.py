4406b1c64c504291b6d8dd7b259abf418654b865
Jagilsdorf <jagilsdorf@gmail.com>
2023-10-05 18:10:11 -0400

    fixed logging

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import os, subprocess, glob, logging; from time import sleep as s
 
-logging.basicConfig(filename='/home/[user]/raspi_video_loop/main_py.log', level=logging.DEBUG)
-logging.info('Script started')
-
 EXTENSIONS = ["mp4", "m4v"]
 current_user = os.getlogin()
 base_path = f"/media/{current_user}"
+
+logging.basicConfig(filename=f'/home/{current_user}/raspi_video_loop/main_py.log', level=logging.DEBUG)
+logging.info('Script started')
+
 
 def start_screensaver():
     #os.system() waits for screensaver to complete before continuing. Using subprocees instead.
